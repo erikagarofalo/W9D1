5c1af140c436385b58a8c8c8255cf7def5de59fe
erikagarofalo <erika.garofalo@gmail.com>
2024-10-07 15:52:29 +0200

    Esercizio Typescript

---
+++ telefono.js
@@ -0,0 +1,55 @@
+// Funzione per convertire il saldo residuo in euro formattato
+function convertiEuro(valore) {
+  return valore.toFixed(2);
+}
+var User = /** @class */ (function () {
+  function User(_nome, _cognome) {
+    this.nome = _nome;
+    this.cognome = _cognome;
+    this.credito = 0;
+    this.numeroChiamate = 0;
+  }
+  User.prototype.ricarica = function (ammontare) {
+    this.credito += ammontare;
+  };
+  User.prototype.chiamata = function (minuti) {
+    var costoChiamata = 0.2;
+    var costoTotale = costoChiamata * minuti;
+    if (costoTotale <= this.credito) {
+      this.credito -= costoTotale;
+      this.numeroChiamate += minuti;
+    } else {
+      console.log("Credito insufficente");
+    }
+  };
+  User.prototype.chiamata404 = function () {
+    return this.credito;
+  };
+  User.prototype.getNumeroChiamata = function () {
+    return this.numeroChiamate;
+  };
+  User.prototype.azzeraChiamate = function () {
+    this.numeroChiamate = 0;
+  };
+  return User;
+})();
+//----------------------------------------
+console.log("\n");
+console.log("Esercizio telefono");
+console.log("\n");
+var utente1 = new User("Erika", "Garofalo");
+// Ricarica del credito dell'utente
+utente1.ricarica(10);
+// Chiamate dell'utente
+utente1.chiamata(8);
+utente1.chiamata(4);
+// Verifico il saldo residuo e il numero di chiamate effettuate
+console.log("All'utente ".concat(utente1.nome, " ").concat(utente1.cognome, " gli rimangono: ").concat(convertiEuro(utente1.chiamata404()), " \u20AC"));
+console.log(
+  "Il numero delle chiamate effettuate da ".concat(utente1.nome, " ").concat(utente1.cognome, " \u00E8: ").concat(utente1.getNumeroChiamata(), " minuti")
+);
+// Azzero le chiamate
+utente1.azzeraChiamate();
+// Verifico quanto gha l'utente dopo aver azzerato
+console.log("Adesso l'utente ".concat(utente1.nome, " ").concat(utente1.cognome, " ha: ").concat(utente1.getNumeroChiamata(), " minuti"));
+console.log("\n");
